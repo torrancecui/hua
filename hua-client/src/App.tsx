37a2415472ff6adcbd8076b4e64597d15972b198
Torrance Cui <cuitorrance@gmail.com>
2022-11-20 18:55:26 -0500

    init

---
+++ hua-client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <h1 >Hello world!</h1>
+    </div>
+  )
+}
+
+export default App
